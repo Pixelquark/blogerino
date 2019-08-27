6129c212f89599a2f99e6d52bba77eadb05345d9
unknown <erikm.pix@gmail.com>
2019-08-27 11:43:42 +0200

    Development

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+
+  <title>Pixel Quark</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Erik Morelli">
+  <meta name="description" content="Practicing CMS development with a simple blog">
+
+  <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="header">
+    <div class="header-title">
+      <h1>Erik Morelli</h1>
+      <p>Webdeveloper @ Pixelquark</p>
+    </div>
+
+    <div class="header-social">
+      <a href=""><img src="assets/linkedin.png" alt=""></a>
+      <a href=""><img src="assets/github.png" alt=""></a>
+      <a href=""><img src="assets/twitter.png" alt=""></a>
+    </div>
+
+    <div class="header-menu">
+      <ul>
+        <a href=""><li>Articles</li></a>
+        <a href=""><li>About</li></a>
+      </ul>
+    </div>
+  </div>
+
+
+  <script src="js/scripts.js"></script>
+</body>
+</html>
