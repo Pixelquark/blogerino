082822914483f3f7bc268490e2ccf71cf3c957fa
unknown <erikm.pix@gmail.com>
2019-09-11 07:12:11 +0200

    development

--- index.php
+++ index.php
@@ -11,31 +11,53 @@
   <meta name="description" content="Practicing CMS development with a simple blog">
 
   <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+
   <div class="header">
+
     <div class="header-title">
       <h1>Erik Morelli</h1>
       <p>Webdeveloper @ Pixelquark</p>
     </div>
 
-    <div class="header-social">
-      <a href=""><img src="assets/linkedin.png" alt=""></a>
-      <a href=""><img src="assets/github.png" alt=""></a>
-      <a href=""><img src="assets/twitter.png" alt=""></a>
-    </div>
 
     <div class="header-menu">
       <ul>
-        <a href=""><li>Articles</li></a>
+        <a href=""><li>Webapps</li></a>
         <a href=""><li>About</li></a>
       </ul>
     </div>
+
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="header-content">
+          <span>Recent Articles</span>
+        </div>
+        <hr><br>
+      </div>
+    </div>
+
+    <div class="col-lg-12">
+      <span class="content-date">11/09/2019</span><br>
+      <h2 class="content-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+      <p class="content-summary">
+        Fusce tempus consequat lorem, at iaculis risus vestibulum quis. Phasellus quis efficitur lectus, in dignissim ligula.
+        Maecenas finibus leo quis iaculis dapibus. Proin nec nisl ac ante congue accumsan vel suscipit mi. Vestibulum ac tempor libero.
+        Morbi sit amet elit odio. Nulla ut pretium nulla.
+      </p>
+      <br><hr>
+    </div>
+
   </div>
 
 
-  <script src="js/scripts.js"></script>
+  <!-- <script src="js/scripts.js"></script> -->
 </body>
 </html>
