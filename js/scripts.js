423443b95088699c89f6794c437aa06a3f58fd4e
unknown <erikm.pix@gmail.com>
2019-09-18 22:16:46 +0200

    development

--- js/scripts.js
+++ js/scripts.js
@@ -4,33 +4,40 @@ $(".header, .nav-social").addClass("animated fadeInDown");
 $(".post-container").delay(1000).fadeIn(400);
 
 
-$("#menu").click(function(){
 
-  $("#menu").removeClass("rotate-center");
-  $("#navmenu").removeClass("slide-in-left");
 
-  if(!$("#navmenu").is(":visible")){
+$('#menu').bind('click', menuAni);
 
-      if($(window).width()<550){
-        $(".header-title").fadeOut(200);
-        $("#navmenu").css({"margin-right":"3em"});
-        if($(window).width()<400){
-          $("#navmenu").css({"margin-right":"2em"});
+function menuAni(){
+    $("#menu").removeClass("rotate-center");
+    $("#navmenu").removeClass("slide-in-left");
+
+    if(!$("#navmenu").is(":visible")){
+
+        if($(window).width()<550){
+          $(".header-title").fadeOut(200);
+          $("#navmenu").css({"margin-right":"3em"});
+          if($(window).width()<400){
+            $("#navmenu").css({"margin-right":"2em"});
+          }
         }
-      }
 
-      $("#menu").addClass("rotate-center");
-      $("#navmenu").delay(200).addClass("slide-in-left");
-      $("#navmenu").fadeIn(400);
+        $("#menu").addClass("rotate-center");
+        $("#navmenu").delay(200).addClass("slide-in-left");
+        $("#navmenu").fadeIn(400);
 
-  }else if($("#navmenu").is(":visible")){
-      if($(window).width()<550){
-        $(".header-title").fadeIn(400);
-        $("#navmenu").css({"margin-right":"0"})
-      };
 
-      $("#menu").addClass("rotate-center");
-      $("#navmenu").fadeOut(1).hide(100);
 
-  }
-});
+
+    }else if($("#navmenu").is(":visible")){
+        if($(window).width()<550){
+          $(".header-title").fadeIn(400);
+          $("#navmenu").css({"margin-right":"0"})
+        };
+
+        $("#menu").addClass("rotate-center");
+        $("#navmenu").fadeOut(1).hide(100);
+
+    }
+
+}
